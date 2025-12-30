29506a3a7d5cb9afba396b42667474e3dfc68fc5
ztsinghua9 <1138596805@qq.com>
2025-12-30 16:21:08 +0800

    训练lolv2-real的超参

---
+++ 1.sh
@@ -0,0 +1,6 @@
+python train.py --cropSize 400 --batchSize 6 --dataset lol_v1
+python train.py --cropSize 400 --batchSize 6 --dataset lol_v1
+python train.py --cropSize 400 --batchSize 6 --dataset lol_v1
+python train.py --cropSize 400 --batchSize 6 --dataset lolv2_real
+python train.py --cropSize 400 --batchSize 6 --dataset lolv2_real
+python train.py --cropSize 400 --batchSize 6 --dataset lolv2_real
